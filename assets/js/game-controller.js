c37363eb2aea88bf6af7d4a8e8b41ee3f45a50d9
Sawyer Warden <sawyer.warden@gmail.com>
2018-09-27 14:18:03 -0400

    More style updates

--- assets/js/game-controller.js
+++ assets/js/game-controller.js
@@ -108,7 +108,7 @@ export default class GameController extends Component {
     // Render page with cards and reset button
     return (
       <div className="GameController" id="game-controller">
-        <div className="header"><h1>React Memory Game!</h1></div>
+        <div className="Header"><h1>React Memory Game!</h1></div>
         {this.createCardRender(this.state.cards)}
         <div id="board">
           <button onClick={this.resetGame}>{resetButtonText}</button>
